dc3957e9d2c9d6a35e74c99007d44e529aae86ed
ammardeche <bibocrew135@gmail.com>
2023-12-12 23:53:44 +0100

    upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,23 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { NotificationComponent } from './pages/notification/notification.component';
+import { PostComponent } from './pages/post/post.component';
+import { TimelineComponent } from './pages/time-line/timeline.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LeftSideBarComponent } from './layout/left-side-bar/left-side-bar.component';
 export const routes: Routes = [
 
 
-   { path: '', loadComponent : ()=> import('./pages/auth/login/login.component').then(log=> log.LoginComponent)},
-   { path: 'layout', component : LayoutComponent}
+   { path: 'login', loadComponent : ()=> import('./pages/auth/login/login.component').then(log=> log.LoginComponent)},
+   
+   { path: '', 
+   component : LayoutComponent,
+   children :[
+      {path: '', component:NotificationComponent},
+      {path: 'post', component:PostComponent},
+      {path: 'timeline', component:TimelineComponent},
+   ]
+   }
 
 ];
 
---
+++ src/app/pages/call-us/callus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-callus',
+  standalone: true,
+  imports: [],
+  templateUrl: './callus.component.html',
+  styleUrl: './callus.component.css'
+})
+export class CallusComponent {
+
+}
