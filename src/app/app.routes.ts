7531764767ddaee08879bfe0071c0debe876f37e
ammardeche <bibocrew135@gmail.com>
2023-12-12 23:26:23 +0100

    upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 export const routes: Routes = [
 
 
-   { path: '', loadComponent : ()=> import('./pages/auth/login/login.component').then(log=> log.LoginComponent)}
+   { path: '', loadComponent : ()=> import('./pages/auth/login/login.component').then(log=> log.LoginComponent)},
+   { path: 'layout', component : LayoutComponent}
 
 ];
 
